2a634a1127052da4fc944d8acdfd097d2e0a9dc7
IhorK <iggy.imj@gmail.com>
2025-07-29 13:02:04 +0200

    Initial commit

---
+++ src/db/schema.ts
@@ -0,0 +1,18 @@
+import { integer, varchar } from "drizzle-orm/pg-core";
+import { pgTable } from "drizzle-orm/pg-core";
+
+export const sportsTable = pgTable("sports", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  title: varchar({ length: 255 }).notNull().unique(),
+  image: varchar({ length: 255 }).notNull(),
+  description: varchar({ length: 255 }).notNull(),
+});
+
+// npx drizzle-kit push
+// supabase postgres
+
+export const eventsTable = pgTable("events", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  name: varchar({ length: 100 }).notNull(),
+  description: varchar({ length: 255 }).notNull(),
+});
